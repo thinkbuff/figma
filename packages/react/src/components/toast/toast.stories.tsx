9571bcc46f571062057d70b06331b525bf99667f
Eleven <eteplus@outlook.com>
2024-04-13 21:02:47 +0800

    feat(react/toast): custom toast classNames

--- packages/react/src/components/toast/toast.stories.tsx
+++ packages/react/src/components/toast/toast.stories.tsx
@@ -30,13 +30,12 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render: () => {
     return (
-      <div className="flex items-center gap-2">
+      <div className="flex flex-wrap items-center gap-2">
         <Button
           variant="outline"
           onClick={() =>
             toast('Event has been created', {
               duration: 1_000_000,
-              dismissible: false,
             })}
         >
           Default
@@ -47,18 +46,136 @@ export const Default: Story = {
             toast('Event has been created', {
               description: 'Monday, January 3rd at 6:00pm',
               duration: 1_000_000,
-              dismissible: false,
               action: {
                 label: 'Undo',
                 onClick: () => console.log('Undo'),
               },
+            })}
+        >
+          Action
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() =>
+            toast('Event has been created', {
+              description: 'Monday, January 3rd at 6:00pm',
+              duration: 1_000_000,
               cancel: {
                 label: 'Cancel',
                 onClick: () => console.log('Cancel'),
               },
             })}
         >
-          Action
+          Cancel
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() =>
+            toast.success('Event has been created', {
+              description: 'Monday, January 3rd at 6:00pm',
+              duration: 1_000_000,
+              cancel: {
+                label: 'Cancel',
+                onClick: () => console.log('Cancel'),
+              },
+            })}
+        >
+          Success
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() =>
+            toast.info('Event has been created', {
+              description: 'Monday, January 3rd at 6:00pm',
+              duration: 1_000_000,
+              cancel: {
+                label: 'Cancel',
+                onClick: () => console.log('Cancel'),
+              },
+            })}
+        >
+          Info
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() =>
+            toast.warning('Event has been created', {
+              description: 'Monday, January 3rd at 6:00pm',
+              duration: 1_000_000,
+              cancel: {
+                label: 'Cancel',
+                onClick: () => console.log('Cancel'),
+              },
+            })}
+        >
+          Warning
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() =>
+            toast.error('Event has been created', {
+              description: 'Monday, January 3rd at 6:00pm',
+              duration: 1_000_000,
+              action: {
+                label: 'Cancel',
+                onClick: () => console.log('Cancel'),
+              },
+            })}
+        >
+          Error
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            toast.promise(
+              new Promise<{ name: string }>(resolve =>
+                setTimeout(() => resolve({ name: 'Figma' }), 2000),
+              ),
+              {
+                loading: 'Loading...',
+                description: () => {
+                  return 'Monday, January 3rd at 6:00pm';
+                },
+                success: (data) => {
+                  return `${data.name} toast has been added`;
+                },
+                error: 'Error',
+              },
+            );
+          }}
+        >
+          Promise
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            toast.loading('Loading toast...', {
+              duration: 2000,
+              description: 'Monday, January 3rd at 6:00pm',
+              cancel: {
+                label: 'Cancel',
+                onClick: () => console.log('Cancel'),
+              },
+            });
+          }}
+        >
+          Loading
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            toast('Event has been created', {
+              description: 'Monday, January 3rd at 6:00pm',
+              duration: 6000,
+              closeButton: true,
+              action: {
+                label: 'Undo',
+                onClick: () => console.log('Undo'),
+              },
+            });
+          }}
+        >
+          CloseButton
         </Button>
       </div>
     );
--- packages/react/src/components/toast/toaster.tsx
+++ packages/react/src/components/toast/toaster.tsx
@@ -1,26 +1,36 @@
-import { Toaster as Sonner } from 'sonner';
+import { Toaster as SonnerToaster } from 'sonner';
 
-type ToasterProps = React.ComponentProps<typeof Sonner>;
+import { buttonVariants } from '../button/button-variants';
+import { cn } from '../../utils';
+
+type ToasterProps = React.ComponentProps<typeof SonnerToaster>;
 
 /**
  * An opinionated toast component for React.
  *
  * - [Docs](https://sonner.emilkowal.ski/)
  */
-const Toaster = ({ ...props }: ToasterProps) => {
+const Toaster = ({ className, ...props }: ToasterProps) => {
   return (
-    <Sonner
-      className="toaster group"
+    <SonnerToaster
+      className={cn('toaster', 'group', className)}
       visibleToasts={2}
+      gap={12}
+      position="bottom-center"
       toastOptions={{
-        // unstyled: true,
+        unstyled: true,
         classNames: {
-          toast:
-            'group toast flex items-center gap-2 p-3 font-size-11 bg-figma text-figma shadow-lg flex items-center border-solid border border-figma rounded-1 w-[var(--width)]',
+          toast: 'group toast flex items-center gap-2 p-3 font-size-11 bg-figma text-figma shadow-lg flex items-center border-solid border border-figma rounded-1 w-[var(--width)]',
           title: 'font-size-12',
-          description: 'font-size-11 text-figma-secondary',
-          actionButton:
-            'group-[.toast]:bg-figma-brand group-[.toast]:text-figma-onbrand',
+          description: 'font-size-11 text-figma-secondary group-data-[type=success]:text-figma-success-tertiary group-data-[type=info]:text-figma-brand-tertiary group-data-[type=warning]:text-figma-warning-tertiary group-data-[type=error]:text-figma-danger-tertiary dark:group-data-[type=warning]:text-figma-onwarning dark:group-data-[type=danger]:text-figma-ondanger',
+          success: 'bg-figma-success-tertiary text-figma-success border-figma-success',
+          info: 'bg-figma-brand-tertiary text-figma-brand border-figma-brand',
+          warning: 'bg-figma-warning-tertiary text-figma-warning border-figma-warning dark:(bg-figma-warning-secondary text-figma-onwarning border-figma-onwarning)',
+          error: 'bg-figma-danger-tertiary text-figma-danger border-figma-danger dark:(bg-figma-danger-secondary text-figma-ondanger border-figma-ondanger)',
+          actionButton: cn(buttonVariants({ color: 'secondary' }), 'h-7 ml-auto'),
+          cancelButton: cn(buttonVariants({ color: 'secondary' }), 'h-7 ml-auto'),
+          closeButton: 'text-figma cursor-default bg-figma-secondary hover:bg-figma-tertiary! active:bg-figma-tertiary! border border-solid border-figma right-0 left-unset translate-x-2/5 -translate-y-2/5 opacity-0 group-hover:opacity-100',
+          icon: '[&_.sonner-loading-bar]:bg-figma-text',
         },
       }}
       {...props}
