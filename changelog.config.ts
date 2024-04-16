fc7a949b2d419e5b5fe6864839edf59b8bd39137
Eleven <eteplus@outlook.com>
2024-04-16 10:54:35 +0800

    build: use `changelogen` to bump version and generate changelog

---
+++ changelog.config.ts
@@ -0,0 +1,16 @@
+export default {
+  types: {
+    feat: { title: "🚀 Features", semver: "minor" },
+    perf: { title: "🔥 Performance", semver: "patch" },
+    fix: { title: "🐛 Fixes", semver: "patch" },
+    refactor: { title: "🔨 Refactors", semver: "patch" },
+    docs: { title: "📖 Documentation", semver: "patch" },
+    build: { title: "📦 Build", semver: "patch" },
+    types: { title: "📝 Types", semver: "patch" },
+    chore: { title: "🧹 Chore" },
+    examples: { title: "📚 Examples" },
+    test: { title: "✅ Tests" },
+    style: { title: "🎨 Styles" },
+    ci: { title: "🤖 CI" },
+  },
+}
---
+++ scripts/update-version.ts
@@ -0,0 +1,12 @@
+import fs from 'node:fs';
+import { glob } from 'glob';
+
+import { version } from '../package.json';
+
+const files = glob.sync('packages/*/package.json');
+
+for (const file of files) {
+  const pkg = JSON.parse(fs.readFileSync(file, 'utf8'));
+  pkg.version = version;
+  fs.writeFileSync(file, JSON.stringify(pkg, null, 2), 'utf8');
+}
